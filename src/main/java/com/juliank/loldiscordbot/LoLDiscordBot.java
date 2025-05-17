3d4a4f411f2eaaa36c04e5486e6626b4b03c7c1e
juliank <julian.krannich1@gmail.com>
2025-05-17 16:22:37 +0200

    Online Status event and ping pong

--- src/main/java/com/juliank/loldiscordbot/LoLDiscordBot.java
+++ src/main/java/com/juliank/loldiscordbot/LoLDiscordBot.java
@@ -4,8 +4,12 @@ import com.juliank.loldiscordbot.listeners.EventListener;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class LoLDiscordBot {
 
@@ -19,6 +23,10 @@ public class LoLDiscordBot {
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.streaming("Fraudulent fiddle", "https://twitch.tv/playcabex"));
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+        builder.enableCache(CacheFlag.ONLINE_STATUS);
         shardManager = builder.build();
 
         shardManager.addEventListener(new EventListener());
--- src/main/java/com/juliank/loldiscordbot/listeners/EventListener.java
+++ src/main/java/com/juliank/loldiscordbot/listeners/EventListener.java
@@ -1,9 +1,15 @@
 package com.juliank.loldiscordbot.listeners;
 
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.events.user.update.UserUpdateOnlineStatusEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.List;
 
 public class EventListener extends ListenerAdapter {
 
@@ -16,6 +22,36 @@ public class EventListener extends ListenerAdapter {
 
         String message = user.getAsTag() + " reacted to a message with " + emoji + " in the " + channelMention + " channel";
         event.getGuild().getDefaultChannel().asTextChannel().sendMessage(message).queue();
+    }
 
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+
+        if(message.contains("ping")) {
+            event.getChannel().sendMessage("pong").queue();
+        }
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        String avatar = event.getUser().getEffectiveAvatarUrl();
+        System.out.println(avatar);
+    }
+
+    @Override
+    public void onUserUpdateOnlineStatus(UserUpdateOnlineStatusEvent event) {
+        List<Member> members = event.getGuild().getMembers();
+        int onlineMembers = 0;
+        for (Member member : members ) {
+            if (member.getOnlineStatus() == OnlineStatus.ONLINE) {
+                onlineMembers++;
+            }
+        }
+
+        User user = event.getUser();
+        String message = "**" + user.getAsTag() + "** updated their status to: " + event.getNewOnlineStatus().getKey() + "! " +
+                "There are now " + onlineMembers + " users online.";
+        event.getGuild().getDefaultChannel().asTextChannel().sendMessage(message).queue();
     }
 }
